5914cd6507d68023ffa684975f1bab55014f15d8
WhiteWolf <marc@rimbaud.net>
2026-03-02 14:10:25 +0100

    lib avec variables

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,12 @@
 #!/usr/bin/env groovy
 import groovy.json.*
 def call(body) {
-	println("Hello World !!!")
+	def mapVars = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = mapVars
+	body()
+	def name = mapVars.name
+	def action = mapVars.action
+
+	println(action + " " + name + "!!")
 }
